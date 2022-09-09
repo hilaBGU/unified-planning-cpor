146ae41884122265b11109c02ce8a49c7c64b61b
franklinselva <franklinselva10@gmail.com>
2022-09-09 06:59:00 +0200

    add grpc planner interface

---
+++ unified_planning/grpc/__init__.py
@@ -0,0 +1,6 @@
+from unified_planning.grpc.server import GRPCPlanner
+from unified_planning.grpc.proto_reader import ProtobufReader
+from unified_planning.grpc.proto_writer import ProtobufWriter
+
+
+__all__ = ["GRPCPlanner", "ProtobufReader", "ProtobufWriter"]
